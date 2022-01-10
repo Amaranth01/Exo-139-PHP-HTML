347bc8ee0635c9f2a4ecd757aff64d7857c7695d
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-10 10:39:08 +0100

    Exo 139 ok

--- index.php
+++ index.php
@@ -14,8 +14,12 @@
      ?>
      <ul>
        <?php
-            // TODO votre code ici
+            $chaine = implode(" , ", $students);
+            echo"<li>$chaine</li>"
        ?>
      </ul>
 
     <!-- Incluez le fichier index2.php ici bas de manière a obtenir la suite du code HTML. -->
+  <?php
+        include "./index2.php";
+  ?>
